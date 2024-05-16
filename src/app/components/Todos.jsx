39a8d4f8e4b72a8ef0a27f1efe0fcc461efbe54b
hjohns50 <henryjohnson@comcast.net>
2024-05-15 22:00:07 -0700

    Add Modal

--- src/app/components/Todos.jsx
+++ src/app/components/Todos.jsx
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
 const Todo = ({ userId }) => {
-    const [user, setUser] = useState(null);
+    // const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${userId}`);
-            const data = await response.json();
-            setUser(data);
-        };
-        fetchUser();
-    }, [userId]);
+    // useEffect(() => {
+    //     const fetchUser = async () => {
+    //         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${userId}`);
+    //         const data = await response.json();
+    //         setUser(data);
+    //     };
+    //     fetchUser();
+    // }, [userId]);
 
-    if (!user) {
-        return <div>Loading...</div>;
-    }
+    // if (!user) {
+    //     return <div>Loading...</div>;
+    // }
 
-    return (
-        <div>
-            <h3>Title: {user.title}</h3>
-            <p>Completed: {user.completed.toString()}</p>
-        </div>
-    );
+    // return (
+    //     <div>
+    //         <h3>Title: {user.title}</h3>
+    //         <p>Completed: {user.completed.toString()}</p>
+    //     </div>
+    // );
 };
 
 export default Todo;
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,31 @@
 "use client";
 
+import { useState } from "react";
 import CountButton from "./components/CountButton";
+import OpenModalButton from "./components/OpenModalButton";
+import Modal from "./components/Modal";
 import Todo from "./components/Todos";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <h1>Demo</h1>
       <CountButton />
       <Todo userId={1} />
+      <div>
+        <h1>React Modal Example</h1>
+        <OpenModalButton onClick={openModal} />
+        <Modal isOpen={isModalOpen} onClose={closeModal} />
+      </div>
     </>
   );
 }
-
-
