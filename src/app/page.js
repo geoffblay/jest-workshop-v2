e2c8eba508e2c4bf3f85d9ed7d79b6206a44f2a5
Geoff Blaylock <gblaylock2024@gmail.com>
2024-05-15 22:11:52 -0700

    geoff branch

--- src/app/page.js
+++ src/app/page.js
@@ -3,21 +3,10 @@
 import { createGlobalStyle, styled } from "styled-components";
 import Title from "./components/Title";
 import CountButton from "./components/CountButton";
-import OpenModalButton from "./components/OpenModalButton";
-import Modal from "./components/Modal";
 import Todo from "./components/Todos";
 
 
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <>
       <GlobalStyle />
